842fdada9dbe266d0c28e40489fc50add2b79b5f
ps2018 <buhpro@mail.ru>
2018-02-21 12:10:56 +0300

    изменилось немного

--- Git_test/Form1.cs
+++ Git_test/Form1.cs
@@ -12,11 +12,73 @@ namespace Git_test
 {
     public partial class Form1 : Form
     {
+        public double res1 = 0;
+        public double res2 = 0;
+        public double result = 0;
         public Form1()
         {
             InitializeComponent();
-            
+
             int a = 6000;
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") res1 = Double.Parse(textBox1.Text);
+            if (textBox2.Text != "") res2 = Double.Parse(textBox2.Text);
+            result = res1 + res2;
+            textBox2.Clear();
+            textBox1.Clear();
+            textBox3.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           if (textBox1.Text!="") res1 = Double.Parse(textBox1.Text);
+           if (textBox2.Text != "") res2 = Double.Parse(textBox2.Text);
+            result = res1 * res2;
+            textBox2.Clear();
+            textBox1.Clear();
+            textBox3.Text = result.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") res1 = Double.Parse(textBox1.Text);
+            if (textBox2.Text != "") res2 = Double.Parse(textBox2.Text);
+            result = res1 - res2;
+            textBox2.Clear();
+            textBox1.Clear();
+            textBox3.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "") res1 = Double.Parse(textBox1.Text);
+            if (textBox2.Text != "") res2 = Double.Parse(textBox2.Text);
+            result = res1 / res2;
+            textBox2.Clear();
+            textBox1.Clear();
+            textBox3.Text = result.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_VisibleChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (textBox3.Text != "") textBox1.Text = textBox3.Text;
+            textBox3.Clear();
         }
     }
 }

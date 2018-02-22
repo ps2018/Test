2944740c5e0a209309864583ae272540fcece00e
ps2018 <buhpro@mail.ru>
2018-02-22 15:57:55 +0300

    изменил на 1

--- Git_test/Form1.cs
+++ Git_test/Form1.cs
@@ -20,7 +20,7 @@ namespace Git_test
         {
             InitializeComponent();
 
-            int a = 12000;
+            int a = 1;
         }
         private double resul(string text1, string text2)
         {
